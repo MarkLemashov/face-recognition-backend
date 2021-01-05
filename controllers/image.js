9c7d0a6a3aa674f7401fb51b7a500f681b46ced3
MarkLemashov <marklemashov049@gmail.com>
2021-01-05 18:41:49 +0200

    clarifai api server side

--- controllers/image.js
+++ controllers/image.js
@@ -2,6 +2,7 @@ const imagePut = (db, clarifai) => (req, res) => {
     const { email, image_url } = req.body;
     clarifai.models.predict(Clarifai.FACE_DETECT_MODEL, image_url)
         .then(response => {
+            console.log(response);
             res.status(200).send(response);
             db('users').where('email', email).increment({entries: 1}).then();
           }
