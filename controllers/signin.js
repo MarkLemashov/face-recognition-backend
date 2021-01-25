3023c813ea2c1652a181d100c25dc15e65cb0e69
MarkLemashov <marklemashov049@gmail.com>
2021-01-25 17:20:12 +0200

    fixed erroe messages

--- controllers/signin.js
+++ controllers/signin.js
@@ -5,19 +5,19 @@ const signinPost = (db, bcrypt) => (req, res) => {
             if(data.length > 0){
                 bcrypt.compare(password, data[0].hash).then(function(result, err) {
                     if(err){
-                        res.status(400).json(err);
+                        res.status(404).json(err);
                     }
 
                     if(result) {
                         db.select('*').from('users').where('email', email).then(user => res.status(200).json(user));
                     }
                     else {
-                        res.status(400).json('Incorrect credentials');
+                        res.status(404).json('Incorrect credentials');
                     }
                 });
             }
             else {
-                res.status(400).json('Incorrect credentials');
+                res.status(404).json('Incorrect credentials');
             }
         })
 };
