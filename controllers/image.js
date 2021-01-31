1a95223f918b4201f59dcef4f4e936e3a57c3d63
MarkLemashov <marklemashov049@gmail.com>
2021-01-31 14:04:51 +0200

    added faces detected counter

--- controllers/image.js
+++ controllers/image.js
@@ -2,8 +2,12 @@ const imagePut = (db, clarifai) => (req, res) => {
     const { email, image_url } = req.body;
     clarifai.models.predict(Clarifai.FACE_DETECT_MODEL, image_url)
         .then(response => {
+
+            faces_detected = response.outputs[0].data.regions.length;
+
+
             res.status(200).json(response);
-            db('users').where('email', email).increment({entries: 1}).then();
+            db('users').where('email', email).increment({entries: 1, faces_detected: faces_detected}).then();
           }
         )
         .catch(err => res.status(400).json('image link error'));
