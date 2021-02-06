9c2b00b6fc527dba7feeb67333c3e96ab6447d50
MarkLemashov <marklemashov049@gmail.com>
2021-02-06 20:29:14 +0200

    roll back

--- controllers/image.js
+++ controllers/image.js
@@ -1,6 +1,6 @@
 const imagePut = (db, clarifai) => (req, res) => {
     const { image_url } = req.body;
-    const email = req.user;
+    const email = req.user.email;
     clarifai.models.predict(Clarifai.FACE_DETECT_MODEL, image_url)
         .then(response => {
             num_of_faces_detected = response.outputs[0].data.regions.length;
@@ -8,7 +8,7 @@ const imagePut = (db, clarifai) => (req, res) => {
             // res.status(200).json(response.outputs[0].data.regions); 
             db('users').where('email', email).increment({entries: 1, faces_detected: num_of_faces_detected}).then( data => {
                 db.select('*').from('users').where('email', email).then(user => {
-                    res.status(200).json({user: email, regions: response.outputs[0].data.regions});
+                    res.status(200).json({user: user[0], regions: response.outputs[0].data.regions});
                 }).catch(err => res.status(400).json('db error 2'));
                 
             }).catch(err => res.status(400).json('db error 1'));
