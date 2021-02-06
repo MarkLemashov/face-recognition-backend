eb82e51ae30b48652290656437561990c4cd0406
MarkLemashov <marklemashov049@gmail.com>
2021-02-06 21:09:28 +0200

    roll back

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ const authenticateToken = (req, res, next) => {
   if (token == null) return res.sendStatus(401);
 
   jwt.verify(token, ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
+    if (err) return res.sendStatus(401);
     req.user = user;
     next();
   })
