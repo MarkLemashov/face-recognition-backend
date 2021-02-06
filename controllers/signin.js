ef15ccabb461bf9e6c24d299b6f548e4bcb724b2
MarkLemashov <marklemashov049@gmail.com>
2021-02-06 18:53:51 +0200

    roll back

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,4 +1,4 @@
-const signinPost = (db, bcrypt) => (req, res) => {
+const signinPost = (db, bcrypt, jwt, ACCESS_TOKEN_SECRET) => (req, res) => {
     const {email, password} = req.body
     db.select('hash').from('login').where('email', email)
         .then(data => {
@@ -9,7 +9,11 @@ const signinPost = (db, bcrypt) => (req, res) => {
                     }
 
                     if(result) {
-                        db.select('*').from('users').where('email', email).then(user => res.status(200).json(user));
+                        db.select('*').from('users').where('email', email).then(user => {
+                            // res.status(200).json(user);
+                            const accessToken = jwt.sign(user, ACCESS_TOKEN_SECRET);
+                            res.status(200).json({acessToken: accessToken});
+                        });
                     }
                     else {
                         res.status(404).json('Incorrect credentials');
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const signin = require('./controllers/signin');
 const register = require('./controllers/register');
 const image = require('./controllers/image');
 const Clarifai = require('clarifai');
+const jwt = require('jsonwebtoken');
 const db = require('knex')({
     client: 'pg',
     connection: {
@@ -20,8 +21,9 @@ const clarifai = new Clarifai.App({
     apiKey: '585d42f3bab543ebb7c64425508f87fc'
    });
 
-let port = process.env.PORT;
-
+const ACCESS_TOKEN_SECRET = '9ef328c25aa478187a9603366154967574767e870452f8e74f7dc9a18bc44bc1dc27d69b6757a8e8c65e4317ebba4197405cf7b4774831828d279ab8164bbbdc';
+const REFRESH_TOKEN_SECRET = '49d6fe9cd576e9a04712c30e8a0c21a9bd9020ebaf36d27f60b8c3a74f12f6a3ce627c043c80ffdcd47ce77872103b0ffa8464b8a545ab1985729ef19317aa2c';
+let PORT = process.env.PORT;
 const saltRounds = 2;
 const app = express();
 
@@ -36,12 +38,12 @@ app.get('/', (req, res) => {
     });
 })
 
-app.post('/signin/', signin.signinPost(db, bcrypt));
+app.post('/signin/', signin.signinPost(db, bcrypt, jwt, ACCESS_TOKEN_SECRET));
 
 app.post('/register/', register.registerPost(db, bcrypt, saltRounds));
 
 app.put('/image', image.imagePut(db, clarifai));
 
-app.listen(port, () => {
-    console.log(`app is running on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`app is running on port ${PORT}`);
 })
