fcc57fd6644b62d6874e6b43e30867f638e3f006
MarkLemashov <marklemashov049@gmail.com>
2021-02-06 20:20:37 +0200

    roll back

--- controllers/image.js
+++ controllers/image.js
@@ -9,8 +9,8 @@ const imagePut = (db, clarifai) => (req, res) => {
             db('users').where('email', email).increment({entries: 1, faces_detected: num_of_faces_detected}).then( data => {
                 db.select('*').from('users').where('email', email).then(user => {
                     res.status(200).json({user: user[0], regions: response.outputs[0].data.regions});
-                }).catch(err => res.status(400).json('db error'));
-            }).catch(err => res.status(400).json('db error'));
+                }).catch(err => res.status(400).json('db error 2'));
+            }).catch(err => res.status(400).json('db error 1'));
           }
         )
         .catch(err => res.status(400).json('image link error'));
