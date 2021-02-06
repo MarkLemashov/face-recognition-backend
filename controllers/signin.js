cf39b920472e333cfd450e85ac76ea35ce6fc836
MarkLemashov <marklemashov049@gmail.com>
2021-02-06 19:07:50 +0200

    roll back

--- controllers/signin.js
+++ controllers/signin.js
@@ -11,8 +11,8 @@ const signinPost = (db, bcrypt, jwt, ACCESS_TOKEN_SECRET) => (req, res) => {
                     if(result) {
                         db.select('*').from('users').where('email', email).then(user => {
                             // res.status(200).json(user);
-                            const accessToken = jwt.sign(user, ACCESS_TOKEN_SECRET);
-                            res.status(200).json(user);
+                            const accessToken = jwt.sign({name: 'mark'}, ACCESS_TOKEN_SECRET);
+                            res.status(200).json({accessToken: accessToken});
                         });
                     }
                     else {
